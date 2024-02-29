fc0ffce11f78da53683abfbf18018a7f64329e93
hiagogil <hiago-gil@hotmail.com>
2024-02-29 11:12:18 -0300

    Correção dos casos de testes

--- cypress/support/commands.js
+++
@@ -1,6 +0,0 @@
-Cypress.Commands.add('haveTextIgnoringCase', (selector, expectedText) => {
-    return cy.get(selector).then($el => {
-      const actualText = $el.text().trim();
-      expect(actualText.toLowerCase()).to.equal(expectedText.toLowerCase());
-    });
-  });
